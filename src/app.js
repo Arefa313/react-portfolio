e244dfa216bbdf188b85c739712c757dc1a69847
Arefa313 <arefa.noori.1380@gmail.com>
2024-08-16 17:18:00 +0430

    adding service component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Header  from "./components/header/header"
 import About from "./components/about/about"
+import Service from "./components/service/Service"
 function App(){
     return(
         <>
         <Header />
         <About />
+        <Service />
         </>
     )
 }
