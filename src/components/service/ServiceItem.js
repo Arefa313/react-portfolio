76143e4416eafe5faea4397432423e6a66d08616
Arefa313 <arefa.noori.1380@gmail.com>
2024-08-17 10:55:26 +0430

    d

--- src/components/service/ServiceItem.js
+++ src/components/service/ServiceItem.js
@@ -1,16 +1,16 @@
 import Service from "./Service"
-function ServiceItem(props){
+function ServiceItem({serviceIcon,serviceTitle,serviceInfo}){
   //
 return(
 <div className="col-4">
                 <div className="service-box">
                   <div className="service-icon">
-                    <i className="{props.serviceIcon}"></i>
+                    <i className={serviceIcon}></i>
                   </div>
                   <div className="service-content">
-                    <h3 className="service-title">{props.serviceTitle}</h3>
+                    <h3 className="service-title">{serviceTitle}</h3>
                     <p className="service-info">
-                     {props.serviceInfo}
+                     {serviceInfo}
                     </p>
                   </div>
                 </div>
